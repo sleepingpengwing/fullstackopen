539b94ad91acfd0fd98d9f0e60b45d924a4d7e41
sleepingpengwing <anchetaandrew01@gmail.com>
2024-09-09 22:06:25 -0400

    Added 'Greeting'
    * Added function "Hello" to add greeting
    * Added greeting to input name and age

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,6 +1,6 @@
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
-import { useState } from "react";
+import { JSXElementConstructor, ReactElement, ReactNode, ReactPortal, useState } from "react";
 /* 
   1. Write down the function
     1.1 We need a button
@@ -12,22 +12,39 @@ import { useState } from "react";
 
   3. Use function in app
 */
-
-const Button = (): JSX.Element => {
+const Footer = () => {
+  return (
+    <div>
+      greeting app created by <a href='https://github.com/mluukkai'>mluukkai</a>
+    </div>
+  )
+}
+function Button(): JSX.Element {
   const [count, setcount] = useState<number>(0);
   return (
     <button
       onClick={() => {
         setcount(count + 1);
-      }}
+      } }
     >
       Count is {count}{" "}
     </button>
   );
-};
-
+}
+const Hello = (props: { name: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; age: string | number | boolean | ReactElement<any, string | JSXElementConstructor<any>> | Iterable<ReactNode> | ReactPortal | null | undefined; }) => {
+  console.log(props)
+  return (
+    <div>
+      <p>
+        Hello world {props.name}, you are {props.age} years old
+      </p>
+    </div>
+  )
+}
 export const App = () => {
   const now: string = new Date().toLocaleDateString();
+  const name = 'Peter'
+  const age = 10
 
   return (
     <>
@@ -47,6 +64,12 @@ export const App = () => {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <div>
+        <h1>Greetings</h1>
+        <Hello name='Maya' age={26 + 10} />
+        <Hello name={name} age={age} />
+        <Footer />
+      </div>
     </>
   );
 };
